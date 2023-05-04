5a532dc1e72d6af102bcea07a5bfef9dfa4757c2
marcus_06 <inormalz32@gmail.com>
2023-05-04 15:10:53 +0700

    moving files

--- Template/ModMulLL.cpp
+++
@@ -1,4 +0,0 @@
-ull modmul(ull a, ull b, ull M) {
-	ll ret = a * b - M * (ull)(1.L / M * a * b);
-	return ret + M * (ret < 0) - M * (ret >= (ll)M);
-}
--- Template/ModPow.cpp
+++
@@ -1,6 +0,0 @@
-ull modpow(ull b, ull e, ull mod) {
-	ull ans = 1;
-	for (; e; b = modmul(b, b, mod), e /= 2)
-		if (e & 1) ans = modmul(ans, b, mod);
-	return ans;
-}
