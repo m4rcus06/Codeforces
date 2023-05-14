01533ed6de03ad006dc2b5d3dadceeb4e7b9bf93
marcus_06 <inormalz32@gmail.com>
2023-05-14 20:46:31 +0700

    update Step2-A

--- Edu/Segment Tree - Part 2/Step 1/A - Addition to Segment/main.cpp
+++ Edu/Segment Tree - Part 2/Step 1/A - Addition to Segment/main.cpp
@@ -11,76 +11,39 @@ typedef long long ll;
 typedef pair<int, int> pii;
 typedef pair<ll, ll> pll;
 
-struct Node {
-	ll val = 0;
-	ll lazy = 0;
-};
-
 struct SegmentTree {
 	int size;
-	vector <Node> Tree;
+	vector <ll> Tree;
 
 	void init(int n) {
-		Tree.resize(4 * n);
+		Tree.resize(4 * n, 0);
 	}
 
-	void build(int id, int lx, int rx, vector <ll> &a) {
-		if (lx == rx) {
-			Tree[id].val = a[lx];
-			return;
-		}
-
-		int mx = (lx + rx) >> 1;
-		build(2 * id, lx, mx, a);
-		build(2 * id + 1, mx + 1, rx, a);
-
-		Tree[id].val = Tree[2 * id].val + Tree[2 * id + 1].val;
-	}
-
-	void apply(int id) {
-		ll add = Tree[id].lazy;
-		Tree[2 * id].val += add;
-		Tree[2 * id].lazy += add;
-
-		Tree[2 * id + 1].val += add;
-		Tree[2 * id + 1].lazy += add;
-
-		Tree[id].lazy = 0;
-	}
-
-	void update(int id, int lx, int rx, int u, int v, ll k) {
+	void add(int id, int lx, int rx, int u, int v, ll k) {
 		if (lx > v || rx < u) return;
 		if (lx >= u && rx <= v) {
-			Tree[id].val += k;
-			Tree[id].lazy += k;
+			Tree[id] += k;
 			return;
 		}
 
 		int mx = (lx + rx) >> 1;
-		apply(id);
-		update(2 * id, lx, mx, u, v, k);
-		update(2 * id + 1, mx + 1, rx, u, v, k);
+		add(2 * id, lx, mx, u, v, k);
+		add(2 * id + 1, mx + 1, rx, u, v, k);
+	}
 
-		Tree[id].val = Tree[2 * id].val + Tree[2 * id + 1].val;
+	void add(int l, int r, ll k) {
+		add(1, 1, size, l + 1, r, k);
 	}
 
 	ll query(int id, int lx, int rx, int i) {
 		if (lx > i || rx < i) return 0;
-		if (lx == rx) return Tree[id].val;
+		if (lx == rx) return Tree[id];
 
 		int mx = (lx + rx) >> 1;
 		ll x = query(2 * id, lx, mx, i);
 		ll y = query(2 * id + 1, mx + 1, rx, i);
 
-		return x + y + Tree[id].lazy;
-	}
-
-	void build(vector <ll> &a) {
-		build(1, 1, size, a);
-	}
-
-	void update(int u, int v, ll k) {
-		update(1, 1, size, u + 1, v, k);
+		return (x + y + Tree[id]);
 	}
 
 	ll query(int i) {
@@ -91,25 +54,23 @@ struct SegmentTree {
 void solve() {
 	int n, q;
 	cin >> n >> q;
-	vector <ll> a(n + 1, 0);
 
 	SegmentTree St;
 	St.size = n;
 	St.init(n);
-	St.build(a);
 
 	while (q--) {
 		int type;
 		cin >> type;
 		if (type == 1) {
-			ll l, r, v;
-			cin >> l >> r >> v;
-			St.update(l, r, v);
+			int l, r, k;
+			cin >> l >> r >> k;
+			St.add(l, r, 1LL * k);
 		}
 		else {
-			int i;
-			cin >> i;
-			cout << St.query(i) << '\n';
+			int idx;
+			cin >> idx;
+			cout << St.query(idx) << '\n';
 		}
 	}
 }
